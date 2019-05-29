2197b91639fd34125e6b7cef1dbd710d512478ab
fransisp <fransiskus_p@outlook.com>
2019-05-29 15:46:09 +0200

    Refractor service discovery component and add vertx-config dependency

--- EmployeeAPIGateway/src/main/java/io/vertx/apigateway/ApiGatewayVerticle.java
+++ EmployeeAPIGateway/src/main/java/io/vertx/apigateway/ApiGatewayVerticle.java
@@ -1,17 +1,11 @@
 package io.vertx.apigateway;
 
-import java.util.Set;
-
+import io.vertx.apigateway.component.GatewayServiceDiscovery;
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.impl.ConcurrentHashSet;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.servicediscovery.Record;
-import io.vertx.servicediscovery.ServiceDiscovery;
-import io.vertx.servicediscovery.ServiceDiscoveryOptions;
-import io.vertx.servicediscovery.types.HttpEndpoint;
 
 /**
  * https://www.sczyh30.com/vertx-blueprint-microservice/
@@ -24,66 +18,31 @@ public class ApiGatewayVerticle extends AbstractVerticle {
 	private static final int DEFAULT_PORT = 8080;
 	private static final String DEFAULT_HOSTNAME = "localhost";
 
-	private ServiceDiscovery discovery;
-	protected Set<Record> records = new ConcurrentHashSet<>();
-
+	private GatewayServiceDiscovery apiServiceDiscovery = new GatewayServiceDiscovery(this.vertx, config());
+	
 	// get HTTP host and port from configuration, or use default value
 	String host = config().getString("api.gateway.http.address", DEFAULT_HOSTNAME);
 	int port = config().getInteger("api.gateway.http.port", DEFAULT_PORT);
 
 	@Override
 	public void start(Future<Void> fut) {
+		
+		ConfigRetriever retriever = ConfigRetriever.create(vertx);
 
-		// init service discovery instance
-		discovery = ServiceDiscovery.create(vertx, new ServiceDiscoveryOptions().setBackendConfiguration(config()));
+		//publish employee api
+		apiServiceDiscovery.publishHttpEndpoint("getEmployeeInfo", host, port, config());
 
 		vertx
 		.createHttpServer()
-		.requestHandler(r ->
-		r.response().end("<h1>Hello from my first Vert.x application</h1>"))
+		.requestHandler(r -> r.response().end("<h1>Hello from my first Vert.x application</h1>"))
 		.listen(port, host, result -> {
 			if (result.succeeded()) {
+				apiServiceDiscovery.publishApiGateway(host, port);
 				fut.complete();
 				logger.info("API Gateway is running");
 			} else {
 				fut.fail(result.cause());
 			}
 		});
-	}
-
-	/**
-	 * Publish HTTP endpoint to the service record
-	 * @param name
-	 * @param host
-	 * @param port
-	 * @return
-	 */
-	protected Future<Void> publishHttpEndpoint(String name, String host, int port) {
-		Record record = HttpEndpoint.createRecord(name, host, port, "/",
-				new JsonObject().put("api.name", config().getString("api.name", ""))
-				);
-		return publish(record);
-	}
-
-	/**
-	 * Publish a service with record.
-	 *
-	 * @param record service record
-	 * @return async result
-	 */
-	private Future<Void> publish(Record record) {
-		Future<Void> future = Future.future();
-	    // publish the service
-	    discovery.publish(record, ar -> {
-	      if (ar.succeeded()) {
-	    	  records.add(record);
-	        logger.info("Service <" + ar.result().getName() + "> published");
-	        future.complete();
-	      } else {
-	        future.fail(ar.cause());
-	      }
-	    });
-
-	return future;
 	}
 }
---
+++ EmployeeAPIGateway/src/main/java/io/vertx/apigateway/component/GatewayServiceDiscovery.java
@@ -0,0 +1,72 @@
+package io.vertx.apigateway.component;
+
+import java.util.Set;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.impl.ConcurrentHashSet;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.servicediscovery.Record;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.ServiceDiscoveryOptions;
+import io.vertx.servicediscovery.types.HttpEndpoint;
+
+public class GatewayServiceDiscovery {
+	
+	private static final Logger logger = LoggerFactory.getLogger(GatewayServiceDiscovery.class);
+	
+	private ServiceDiscovery discovery;
+	private Set<Record> records = new ConcurrentHashSet<>();
+	
+	public GatewayServiceDiscovery(Vertx vertx, JsonObject configuration)
+	{
+		// init service discovery instance
+		discovery = ServiceDiscovery.create(vertx, new ServiceDiscoveryOptions().setBackendConfiguration(configuration));
+	}
+	
+	/**
+	 * Publish HTTP endpoint to the service record
+	 * @param name
+	 * @param host
+	 * @param port
+	 * @return
+	 */
+	public Future<Void> publishHttpEndpoint(String name, String host, int port, JsonObject configuration) {
+		Record record = HttpEndpoint.createRecord(name, host, port, "/",
+				new JsonObject().put("api.name", configuration.getString("api.name", ""))
+				);
+		return publish(record);
+	}
+
+
+	public Future<Void> publishApiGateway(String host, int port) {
+		Record record = HttpEndpoint.createRecord("api-gateway", true, host, port, "/", null)
+				.setType("api-gateway");
+		return publish(record);
+	}
+
+	/**
+	 * Publish a service with record.
+	 *
+	 * @param record service record
+	 * @return async result
+	 */
+	private Future<Void> publish(Record record) {
+		Future<Void> future = Future.future();
+		// publish the service
+		discovery.publish(record, ar -> {
+			if (ar.succeeded()) {
+				records.add(record);
+				logger.info("Service <" + ar.result().getName() + "> published");
+				future.complete();
+			} else {
+				future.fail(ar.cause());
+			}
+		});
+
+		return future;
+	}
+
+}
